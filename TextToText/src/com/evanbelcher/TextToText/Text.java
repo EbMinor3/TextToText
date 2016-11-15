3ddebe1c6b47634ea043e93a14549ffbdfb2c01c
EbMinor3 <evanbelcher3@gmail.com>
2016-11-15 12:16:35 -0500

    First commit

---
+++ TextToText/src/com/evanbelcher/TextToText/Text.java
@@ -0,0 +1,34 @@
+package com.evanbelcher.TextToText;
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Text {
+
+	private Date datetime;
+	private String sender;
+	private ArrayList<String> message;
+	
+	public Text(Date datetime, String sender, ArrayList<String> message){
+		this.datetime = datetime;
+		this.sender = sender;
+		this.message = message;
+	}
+
+	
+	public Date getDatetime() {
+		return datetime;
+	}
+
+	
+	public String getSender() {
+		return sender;
+	}
+
+	
+	public ArrayList<String> getMessage() {
+		return message;
+	}
+	
+	
+	
+}
